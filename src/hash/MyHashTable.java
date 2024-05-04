f684372aacbf74375d14b369c42c55af717ae7ad
assyl <zhangubek99@mail.ru>
2024-05-05 00:12:39 +0600

    added methods to MyHashTable class

--- src/hash/MyHashTable.java
+++ src/hash/MyHashTable.java
@@ -1,5 +1,55 @@
 package hash;
 
-public class MyHashTable {
+public class MyHashTable<K, V> {
+    private class Hashnode<K, V> {
+        private K key;
+        private V value;
+
+        public Hashnode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{ " + key + " " + value + "}";
+        }
+    }
+
+    private Hashnode<K, V>[] chainArray;
+    private int defaultCapacity = 15; // default number of chains
+    private int size;
+
+    public MyHashTable() {
+
+    }
+
+    public MyHashTable(int initialCapacity) {
+
+    }
+
+    public int hash(K key) {
+        return 0;
+    }
+
+    public void put(K key, V value) {
+
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public V remove(K key) {
+        return null;
+    }
+
+    public boolean contains(V value) {
+        return false;
+    }
+
+    public K getKey(V value) {
+        return null;
+    }
 
 }
